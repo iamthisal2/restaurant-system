0373c97be355ac007dc654ecc36b830a380e5ead
iamthisal2 <IT24101868@my.sliit.lk>
2025-09-24 21:18:42 +0530

    init commit

---
+++ src/main/java/com/example/demo/dto/CartDto.java
@@ -0,0 +1,29 @@
+package com.example.demo.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CartDto {
+
+
+    private int id;
+    private int userid;
+    private String itemName;
+    private int quantity;
+    private double price;
+
+
+
+
+
+
+
+
+
+
+}
