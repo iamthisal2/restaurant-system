af0a31afbddff7ca09fa84bc3a30274db8c76b0d
iamthisal2 <IT24101868@my.sliit.lk>
2025-10-09 04:47:26 +0530

    removed duplicate files

--- Cart-backend/src/main/java/com/example/demo/repo/CartRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepo extends JpaRepository<Cart,Integer> {
-}
